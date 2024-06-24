7c7bc5929635a11e1d9da3564c1bf3f306270218
TheOrbMaverick <enangita@gmail.com>
2024-06-24 19:27:45 +0100

    eslint checks

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,13 +1,13 @@
 export default function createIteratorObject(report) {
-    const allEmployees = report.allEmployees;
-  
-    function* employeeGenerator() {
-      for (const department of Object.values(allEmployees)) {
-        for (const employee of department) {
-          yield employee;
-        }
+  const allEmployees = report.allEmployees;
+
+  function* employeeGenerator() {
+    for (const department of Object.values(allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
     }
-  
-    return employeeGenerator();
   }
+
+  return employeeGenerator();
+}
