21a10fbb869b1879538e41ca4a560bd211f694a5
TheOrbMaverick <enangita@gmail.com>
2024-06-25 21:24:31 +0100

    npx eslint [file] --fix: to fix all formatting issues

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-    if (success) {
-      return Promise.resolve({
-        status: 200,
-        body: 'Success'
-      });
-    } else {
-      return Promise.reject(new Error('The fake API is not working currently'));
-    }
+  if (success) {
+    return Promise.resolve({
+      status: 200,
+      body: 'Success',
+    });
+  }
+  return Promise.reject(new Error('The fake API is not working currently'));
 }
