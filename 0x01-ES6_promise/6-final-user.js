e30c2cc41a55ed50d784fd011dbda34c8eb42282
TheOrbMaverick <enangita@gmail.com>
2024-06-28 13:22:44 +0100

    why did 6 not pass

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,9 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPhotoPromise = uploadPhoto(fileName).catch((error) => Promise.reject(new Error(`${fileName} cannot be processed`)));
+  const uploadPhotoPromise = uploadPhoto(fileName).catch(() => {
+    Promise.reject(new Error(`${fileName} cannot be processed`));
+  });
 
   return Promise.allSettled([signUpPromise, uploadPhotoPromise])
     .then((results) => results.map((result) => ({
