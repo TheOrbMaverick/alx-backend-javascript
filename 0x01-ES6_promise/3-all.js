18b900217ee089f682811507cdd6c9c744e0a451
TheOrbMaverick <enangita@gmail.com>
2024-06-28 12:27:56 +0100

    why did 3 not pass

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
-      const body = photo.body;
-      const firstName = user.firstName;
-      const lastName = user.lastName;
-      console.log(`${body} ${firstName} ${lastName}`);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
