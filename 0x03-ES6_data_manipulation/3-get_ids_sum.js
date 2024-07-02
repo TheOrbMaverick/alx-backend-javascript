836d55583d8bfee4c7a35038f2893fb2822cd2ff
TheOrbMaverick <enangita@gmail.com>
2024-07-02 17:11:52 +0100

    3-not-checking

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,4 @@
 export default function getStudentIdsSum(studentList) {
-  return studentList.reduce((accumulator, currentValue) => {accumulator + currentValue.id, 0});
+  const idSum = studentList.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
+  return idSum;
 }
