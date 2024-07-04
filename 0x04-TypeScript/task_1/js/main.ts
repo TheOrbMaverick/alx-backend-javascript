1cfd258267c2531c1c6d7a5c07d3109f4eaea456
TheOrbMaverick <enangita@gmail.com>
2024-07-04 11:40:20 +0100

    the tasks are too many to keep up with

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+//TASK 1
 interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
@@ -7,37 +8,119 @@ interface Teacher {
     [key: string]: any;
 }
 
-interface Directors extends Teacher {
+
+//TASK 2
+export interface Directors extends Teacher {
     numberOfReports: number
 }
 
+
+//TASK 3
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
 // Implement the function
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+export const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
 
-interface StudentClass {
-    firstName: string;
-    lastName: string;
+
+
+//TASK 4
+// Interface for the StudentClass constructor
+interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
 }
 
-class StudentClass {
-    firstName;
-    lastName;
+// Interface for the StudentClass methods
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+// Implementing the StudentClass
+export class StudentClass implements StudentClassInterface {
+    private firstName: string;
+    private lastName: string;
+
     constructor(firstName: string, lastName: string) {
-        this.firstName = firstName
-        this.lastName = lastName
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     workOnHomework(): string {
-        return "Currently working"
+        return 'Currently working';
     }
 
     displayName(): string {
-        return this.firstName
+        return this.firstName;
+    }
+}
+
+
+//TASK 5
+// DirectorInterface with the expected methods
+export interface DirectorInterface {
+    workFromHome: () => string;
+    getCoffeeBreak: () => string;
+    workDirectorTasks: () => string;
+}
+
+// TeacherInterface with the expected methods
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+}
+
+export class Director implements DirectorInterface {
+
+    workFromHome(): string {
+        return "Working from home"
+    }
+
+    getCoffeeBreak(): string {
+        return "Getting a coffee break"
+    }
+
+    workDirectorTasks(): string {
+        return "Getting to director tasks"
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+
+    workTeacherTasks(): string {
+        return 'Getting to work';
+    }
+}
+
+export function createEmployee(salary: string): Director | Teacher {
+
+    const intSalary = parseInt(salary)
+    if (Number.isInteger(intSalary) && intSalary < 500) {
+        return new Teacher
+    }
+    return new Director
+}
+
+
+function isDirector(employee: Director | Teacher): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
     }
 }
