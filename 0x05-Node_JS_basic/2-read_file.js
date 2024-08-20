63d291f20b44ef7bbef1be8cdfbb25967bb79e13
TheOrbMaverick <enangita@gmail.com>
2024-08-20 22:16:03 +0100

    the node js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -14,9 +14,6 @@ function countStudents(path) {
       throw new Error('Cannot load the database');
     }
 
-    // Remove the header line
-    const header = lines.shift();
-
     // Initialize counters and lists for each field
     const students = {};
     let totalStudents = 0;
---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const http = require('http')
+
+const app = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('Hello Holberton School\n')
+});
+
+const PORT = 1245;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+});
+
+module.exports = app
