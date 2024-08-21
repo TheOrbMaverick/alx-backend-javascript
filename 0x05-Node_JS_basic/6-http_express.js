bf8dbdc22e91b8bfb4b2472a3e3a496b168544fe
TheOrbMaverick <enangita@gmail.com>
2024-08-21 02:31:37 +0100

    checking this now

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -5,12 +5,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 const PORT = 1245;
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 module.exports = app;
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,34 +1,34 @@
 #!/usr/bin/node
 
 const express = require('express');
-const countStudents = require('./3-read_file_async.js');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-    const databasePath = process.argv[2];
+  const databasePath = process.argv[2];
 
-    if (!databasePath) {
-        res.status(500).send('Database file path not provided');
-        return;
-    }
+  if (!databasePath) {
+    res.status(500).send('Database file path not provided');
+    return;
+  }
 
-    countStudents(databasePath)
-        .then((output) => {
-            res.send(`This is the list of our students\n${output}`);
-        })
-        .catch((err) => {
-            res.status(500).send(err.message);
-        });
+  countStudents(databasePath)
+    .then((output) => {
+      res.send(`This is the list of our students\n${output}`);
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
 });
 
 const PORT = 1245;
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 module.exports = app;
