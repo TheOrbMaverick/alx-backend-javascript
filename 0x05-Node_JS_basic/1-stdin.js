bd6ddaadb34858fe66ddb744f71ec90bae384c8a
TheOrbMaverick <enangita@gmail.com>
2024-08-23 14:35:32 +0100

    executable

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -11,10 +11,10 @@ const rl = readline.createInterface({
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 rl.on('line', (input) => {
-  process.stdout.write(`Your name is: ${input}\r\n`);
+  process.stdout.write(`Your name is: ${input}\n`);
   rl.close();
 });
 
 rl.on('close', () => {
-  process.stdout.write('This important software is now closing\r\n');
+  process.stdout.write('This important software is now closing\n');
 });
