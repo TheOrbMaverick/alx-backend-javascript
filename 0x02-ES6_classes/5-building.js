266d5008245d5bd7d4d29bd9d226c2d81358a7ae
TheOrbMaverick <enangita@gmail.com>
2024-09-21 21:05:21 +0100

    checker

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,13 @@
 export default class Building {
   constructor(sqft) {
-    // Enforce the abstract class rule
+    // Enforce abstract class - cannot instantiate directly
     if (new.target === Building) {
       throw new Error('Cannot instantiate an abstract class.');
     }
 
     // Ensure sqft is a number
-    if (typeof sqft !== 'number' || Number.isNaN(sqft) || sqft <= 0) {
-      throw new TypeError('Square footage must be a positive number.');
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square footage must be a number.');
     }
 
     this._sqft = sqft;
@@ -18,7 +18,7 @@ export default class Building {
     return this._sqft;
   }
 
-  // Abstract method to be implemented by subclasses
+  // Abstract method that must be implemented by subclasses
   // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
