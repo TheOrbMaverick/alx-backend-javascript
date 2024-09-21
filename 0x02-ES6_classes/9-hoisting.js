e6bce3ddacef4fcba8b7fba45309c59e7a1dc836
TheOrbMaverick <enangita@gmail.com>
2024-09-21 21:45:39 +0100

    02 checkers

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -33,18 +33,22 @@ export class StudentHolberton {
   }
 }
 
-// Creating instances of HolbertonClass
+// Create instances of HolbertonClass
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-// Creating instances of StudentHolberton
+// Create instances of StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', new HolbertonClass(2019, 'San Francisco'));
 const student4 = new StudentHolberton('Donald', 'Bush', new HolbertonClass(2019, 'San Francisco'));
 const student5 = new StudentHolberton('Jason', 'Sandler', new HolbertonClass(2019, 'San Francisco'));
 
+// Create an array of students
 const listOfStudents = [student1, student2, student3, student4, student5];
-console.log(listOfStudents);
-console.log(listOfStudents.map(student => student.fullStudentDescription))
 
-export {listOfStudents};
+// Log the list of students and their descriptions
+console.log(listOfStudents);
+console.log(listOfStudents.map(student => student.fullStudentDescription));
+
+// Ensure that this variable is exported
+export { listOfStudents };
