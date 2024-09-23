77d7c77a3dc62be1ebd6167ff9b3165f45922e15
TheOrbMaverick <enangita@gmail.com>
2024-09-23 19:33:38 +0100

    backend jvascript corrections

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,21 +1,12 @@
 #!/usr/bin/node
 
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false,
-});
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-rl.on('line', (input) => {
-  const cleanedInput = input.replace(/\r/, ''); // Remove carriage return
-  process.stdout.write(`Your name is: ${cleanedInput}\r\n`);
+process.stdout.on('line', (input) => {
+  process.stdout.write(`Your name is: ${input}\n`);
   rl.close();
 });
 
-rl.on('close', () => {
+process.stdout.on('close', () => {
   process.stdout.write('This important software is now closing\n');
 });
