884f4373239520d02c5d72ea7c5fc0fb7f81f4c2
TheOrbMaverick <enangita@gmail.com>
2024-09-23 20:01:49 +0100

    backend jvascript corrections

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  if (!fs.existsSync(dataPath)) {
+  if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
-  if (!fs.statSync(dataPath).isFile()) {
+  if (!fs.statSync(path).isFile()) {
     throw new Error('Cannot load the database');
   }
   try {
