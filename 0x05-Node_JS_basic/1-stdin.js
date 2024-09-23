a50e3f086d4d688ce5f94ff98e1b64114e41680c
TheOrbMaverick <enangita@gmail.com>
2024-09-23 19:39:32 +0100

    backend jvascript corrections

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,8 +3,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdout.on('line', (input) => {
-  process.stdout.write(`Your name is: ${input}\n`);
-  rl.close();
+  process.stdout.write(`Your name is: ${input}`);
 });
 
 process.stdout.on('close', () => {
